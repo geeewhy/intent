edb595b2d7f91499da0c981f0c5de66a8092c59c
Gokce Yalcin <gokceylcn@gmail.com>
2025-05-17 15:15:58 -0700

    build: add jest explicitly to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 npm ci 2>1 > /dev/null
-npm install -g ts-node --save-dev 2>1 > /dev/null
+npm install jest
+npm install -g jest ts-node --save-dev 2>1 > /dev/null
 npm install -g ts-node-dev 2>1 > /dev/null
 echo "OK - Dependencies installed"
