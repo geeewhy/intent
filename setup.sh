a25b6c348488da99722ec8bb6e2ffdb26aa8f664
Gokce Yalcin <gokceylcn@gmail.com>
2025-05-17 15:36:47 -0700

    build: fix stderr stdinput redir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-npm ci 2>1 > /dev/null
-npm install jest
-npm install -g jest ts-node --save-dev 2>1 > /dev/null
-npm install -g ts-node-dev 2>1 > /dev/null
+npm install > /dev/null 2>&1
+npm install jest > /dev/null 2>&1
+npm install -g jest ts-node --save-dev > /dev/null 2>&1
+npm install -g ts-node-dev > /dev/null 2>&1
 echo "OK - Dependencies installed"
