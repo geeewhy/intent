7240b0bb5cfb62f58c668637c2300c20dd144045
Gokce Yalcin <gokceylcn@gmail.com>
2025-05-17 15:08:37 -0700

    build: add a setup.sh see if agent would respond

---
+++ setup.sh
@@ -0,0 +1,4 @@
+npm ci 2>1 > /dev/null
+npm install -g ts-node --save-dev 2>1 > /dev/null
+npm install -g ts-node-dev 2>1 > /dev/null
+echo "OK - Dependencies installed"
--- src/setup.sh
+++
@@ -1,3 +0,0 @@
-npm ci
-npm install -g ts-node --save-dev
-npm install -g ts-node-dev
