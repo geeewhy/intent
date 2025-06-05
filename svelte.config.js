1517b5006c3c6f71e66149e474b372fa43a56fd2
gokce yalcin <gokceylcn@gmail.com>
2025-06-05 07:38:56 -0700

    chore(sidepanel): upgrade sveltekit deps

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,9 @@ import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 const config = {
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: true
+  }),
   kit: {
     adapter: adapter(),
     files: {
