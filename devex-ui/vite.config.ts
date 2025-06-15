6c70a4dbb60f66fb4be31a08660f04f8cb23b1d2
Gokce Yalcin <gokceylcn@gmail.com>
2025-06-14 17:56:52 -0700

    WIP(devx-ui): test remark vite config adjustments

--- devex-ui/vite.config.ts
+++ devex-ui/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { plugin as Markdown } from 'vite-plugin-markdown';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    Markdown({ mode: ['html', 'toc'] }),
   ].filter(Boolean),
   resolve: {
     alias: {
