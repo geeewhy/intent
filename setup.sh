1759e70fa8d815e10796b104025f475e7f13258a
Gokce Yalcin <gokceylcn@gmail.com>
2025-06-22 23:47:18 -0700

    chore(setup): improve setup instructions clarity
    setup.sh: Updated instructions, removed redundant temporal-ui step, added eventstore setup and README.md reference.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ npm install -g ts-node-dev > /dev/null 2>&1
 echo "
 OK - Dependencies installed, next up:
 
-- run docker-compose up temporal-ui temporal
-- run npm run setup
+- run docker-compose up
+- run npm run setup eventstore
 - run npm run projections:migrate
+
+see full setup instructions in README.md
 "
